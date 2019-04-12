11d846f8039a79604337d4fa2642d60dee45ecdb
lmjing <lmjing1234@gmail.com>
2019-04-12 18:30:11 +0900

    검색 폼 구현

--- 1-vanilla/js/app.js
+++ 1-vanilla/js/app.js
@@ -1,5 +1,7 @@
+import FormView from '../views/FormView.js'
 import MainController from './controllers/MainController.js'
 
 document.addEventListener('DOMContentLoaded', () => {
    MainController.init();
+   FormView.setup(document.querySelector('form'));
 });
---
+++ 1-vanilla/js/views/FormView.js
@@ -0,0 +1,30 @@
+import View from './View.js'
+
+const tag = '[MainView]' // 디버깅을 위한 태그
+
+/*
+Object.create의 리턴 값 : 지정된 프로토타입 개체와 속성을 갖는 새로운 개체.
+ */
+const FormView = Object.create(View) // 기존 View를 확장한 새로운 오브젝트 생성
+
+// setup 함수 생성
+FormView.setup = function (el) {
+    // el 검사 및 셋팅
+    this.init(el)
+    /*
+    querySelector() : 제공한 CSS 선택자를 만족하는 첫 번째 Element 객체. 결과가 없다면 null.
+     */
+    // el로 부터 필요한 하위 엘리먼트 가져와 셋팅
+    this.inputEl = el.querySelector('[type=text]')
+    this.resetEl = el.querySelector('[type=reset')
+    // 처음엔 입력 값이 없으니 reset 버튼 안보이도록 함
+    this.showResetButton(false)
+}
+
+// 매개변수의 디폴트 값은 true이다.
+FormView.showResetButton = function (show = true) {
+    // reset 버튼이 스타일 속성을 바꿔준다.
+    this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+export default FormView
