6437c8a87971bd091ac442d40867befb562b1c8c
lmjing <lmjing1234@gmail.com>
2019-04-21 16:03:00 +0900

    검색폼 구현(실습)

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,11 +1,19 @@
 new Vue({
   el: '#app',
   data: {
-      keyword: ''
+      query: ''
   },
     methods: {
       onSubmit(e) {
           debugger
-      }
+      },
+        onReset() {
+          console.log('onReset', this.query)
+          this.query = ''
+        },
+        onKeyup() {
+          console.log('onKeyup', this.query)
+          if (!this.query.length) this.onReset()
+        }
     }
 })
