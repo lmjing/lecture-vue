1a31f6470c7ea31a7946d326f8c0092e00863895
lmjing <lmjing1234@gmail.com>
2019-04-21 17:56:06 +0900

    최근검색어 구현(함께)

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,5 +1,6 @@
 import SearchModel from './models/SearchModel.js'
 import KeywordModel from './models/KeywordModel.js'
+import HistoryModel from './models/HistoryModel.js'
 
 new Vue({
   el: '#app',
@@ -9,17 +10,24 @@ new Vue({
       tabs: ['추천 검색어', '최근 검색어'],
       selectedTab: '', // 여기서 정의하지 않으면 탭 클릭 동작 정확히 되지 않음
       searchResult: [],
-      keywords: []
+      keywords: [],
+      history: []
   },
     created() {
       // lifeCycle 중 처음 생성이 완료되었을 때 호출됨
         this.selectedTab = this.tabs[0]
         this.fetchKeyword()
+        this.fetchHistory()
     },
     methods: {
         fetchKeyword() {
             KeywordModel.list().then(data => {
                 this.keywords = data
+            })
+        },
+        fetchHistory() {
+            HistoryModel.list().then(data => {
+                this.history = data
             })
         },
       onSubmit(e) {
